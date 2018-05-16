c01b01b05fea38b1755825926110e085e955c4f3
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-05-16 04:06:56 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -5,6 +5,9 @@ export default class Spaceship extends React.Component{
     return(
       <div>
         <h1> Hello, this is {props.name} </h1>
+        The speed is {props.speed}
+        Has rockets? {props.hasRockets}
+        What colors? {props.colors}
       </div>
     )
   }
